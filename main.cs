9141290bb8934c013fa4d7cdf216988a35a9525b
richardboateng9 <26073139-richardboateng9@users.noreply.replit.com>
2023-09-05 16:43:13 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program 
+{
+
+      static void Main()
+      {
+           Console.WriteLine("Lucinda Potter");
+           Console.WriteLine("6/24/1992");
+           Console.WriteLine("work 000-000-0101");
+           Console.WriteLine("cell 000-000-0189");
+      }
+  }
+  
